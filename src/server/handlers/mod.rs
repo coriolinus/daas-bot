94940686e8e0f0936d281bdf311a7cdc483719ba
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2026-06-15 02:48:43 +0200

    feat: add helper for ensuring guild-management perms

--- src/server/handlers/mod.rs
+++ src/server/handlers/mod.rs
@@ -5,9 +5,21 @@ mod export;
 mod help;
 mod ping;
 
-pub use cleanup::cleanup;
-pub use disable::disable;
-pub use enable::enable;
-pub use export::export;
-pub use help::help;
-pub use ping::ping;
+use serenity::all::CommandInteraction;
+
+pub use self::{
+    cleanup::cleanup, disable::disable, enable::enable, export::export, help::help, ping::ping,
+};
+use super::{Error, Result};
+
+/// Ensure that the user who created the interaction has "manage guild" permissions
+fn may_manage_guild(interaction: &CommandInteraction) -> Result<bool> {
+    let has_manage_guild_permission = interaction
+        .member
+        .as_ref()
+        .ok_or(Error::MalformedInput("no interaction member"))?
+        .permissions
+        .ok_or(Error::MalformedInput("no member permissions"))?
+        .manage_guild();
+    Ok(has_manage_guild_permission)
+}
