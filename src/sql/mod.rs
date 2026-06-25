332e1fdd8db16a2378d3bc1059f3df17ccea9a35
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2026-06-25 02:18:45 +0200

    refactor: move `ToSqlInteger` to its own module

--- src/sql/mod.rs
+++ src/sql/mod.rs
@@ -1,6 +1,8 @@
 mod bot;
 mod error;
-mod export;
+pub(crate) mod export;
+mod to_sql_integer;
 
 pub use bot::{channel_is_enabled, disable_channel, enable_channel};
 pub use error::{Error, Result};
+pub(crate) use to_sql_integer::ToSqlInteger;
---
+++ src/sql/to_sql_integer.rs
@@ -0,0 +1,26 @@
+use serenity::all::{ChannelId, GuildId, UserId};
+
+pub(crate) trait ToSqlInteger: Sized {
+    /// Transform `self` into a `u64`
+    fn to_u64(self) -> u64;
+
+    /// Transform `self` into a `i64` which sqlite can handle
+    ///
+    /// This is a pure binary transform; we don't care about the value produced, only
+    /// that it fits into an `i64` which is a Sqlite `INTEGER` type.
+    fn to_sql(self) -> i64 {
+        i64::from_ne_bytes(self.to_u64().to_ne_bytes())
+    }
+}
+
+macro_rules! impl_to_sql_integer_via_get {
+    ($( $t:ty ),*) => {
+        $(
+            impl ToSqlInteger for $t {
+                fn to_u64(self) -> u64 { self.get() }
+            }
+        )*
+    };
+}
+
+impl_to_sql_integer_via_get!(ChannelId, GuildId, UserId);
