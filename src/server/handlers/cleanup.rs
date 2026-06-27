318b919bdf4737c1c924065a5672a00ca30fca12
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2026-06-28 01:11:39 +0200

    feat: emit proper "not yet implemented" cleanup response
    Just friendlier behavior for a bot.
    Obviously we need to follow up and implement cleanup, but it doesn't
    have to be right now. We can deploy this to a real machine and not
    feel too guilty.

--- src/server/handlers/cleanup.rs
+++ src/server/handlers/cleanup.rs
@@ -1,13 +1,38 @@
+use either::Either;
 use log::debug;
-use serenity::all::CommandInteraction;
+use serenity::all::{CommandInteraction, CreateInteractionResponseMessage};
 
-use crate::server::{AppState, Defer, Result};
+use crate::{
+    server::{AppState, Defer, Error, Message, Result},
+    sql::channel_is_enabled,
+};
 
 /// Delete all but the most recent export from this server.
 ///
 /// 1. Launch an async task to actually accomplish that.
 /// 2. While that's running, return this response.
-pub async fn cleanup(_interation: CommandInteraction, _app_state: &AppState) -> Result<Defer> {
+pub async fn cleanup(
+    interaction: CommandInteraction,
+    app_state: &AppState,
+) -> Result<Either<Message, Defer>> {
     debug!("handling cleanup interaction");
-    todo!()
+    let guild = interaction
+        .guild_id
+        .ok_or(Error::MalformedInput("no guild id"))?;
+
+    let connection = app_state.local_db.clone().lock_owned().await;
+
+    if !channel_is_enabled(connection, guild, interaction.channel_id).await? {
+        return Ok(Either::Left(Message::from(
+            CreateInteractionResponseMessage::new()
+                .ephemeral(true)
+                .content("permission denied"),
+        )));
+    }
+
+    Ok(Either::Left(Message::from(
+        CreateInteractionResponseMessage::new()
+            .ephemeral(true)
+            .content("🚧 `/daas cleanup` is not yet implemented. ask a channel admin to delete old messages.")
+    )))
 }
